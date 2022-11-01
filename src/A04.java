be1282d068e31a83e3ed0a431531e5cac2e6b931
harunapak <70447221+harunapak@users.noreply.github.com>
2022-11-01 21:13:19 +0100

    5.commit

--- src/A04.java
+++ src/A04.java
@@ -1,5 +1,16 @@
 public class A04 {
     public static void main(String[] args) {
+        /*
+        Bir prijeyi ilk defa olusturdugumuzda
+        1- commit olustur
+        2- uzak masaustunde bir repo olustur
+        3- githubda istenen son 3 satiri copy yapip calistir
+
+        Bir projeyi bir kere uzak masaustu ile iliskilendirdigimiz zaman
+        sonra yapacagimiz her degisilkli icin
+        1- git add . : yeni yapilan degisillikleri git' e ekler
+        2- git commit -m""
+         */
 
 
     }
